5e536a3540da60693df1029f5e13ca2adbc76288
Subhajit Sahu <wolfram77@gmail.com>
2019-04-02 21:59:31 +0530

    is it god?

---
+++ index.js
@@ -0,0 +1,28 @@
+const cp = require('child_process');
+
+
+
+const exec = cp.exec;
+const execFile = cp.execFile;
+
+
+
+function cpExec(command, options, callback) {
+  if(typeof callback==='function' || typeof options==='function') return exec(command, options, callback);
+  return new Promise((fres, frej) => exec(command, options, (err, stdout, stderr) => {
+    return (err? frej:fres)({err, stdout, stderr});
+  }));
+}
+
+function cpExecFile(file, args, options, callback) {
+  if(typeof callback==='function' || typeof options==='function' || typeof args==='function') return execFile(file, args, options, callback);
+  return new Promise((fres, frej) => execFile(file, args, options, (err, stdout, stderr) => {
+    return (err? frej:fres)({err, stdout, stderr});
+  }));
+}
+
+
+
+cp.exec = cpExec;
+cp.execFile = cpExecFile;
+module.exports = cp;
