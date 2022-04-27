5e7fad38bfdf75dcd9026d9299b3296cb97ba2bf
Subhajit Sahu <wolfram77@gmail.com>
2022-04-27 22:47:08 +0530

    :turtle: simpler?

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import * as C from "child_process";
 import {ExecException} from "child_process";
-import {ChildProcess, PromiseWithChild} from "child_process";
+import {ChildProcess, PromiseWithChild}       from "child_process";
 import {ExecSyncOptions, ExecFileSyncOptions} from "child_process";
-import {SpawnSyncOptions, SpawnSyncReturns} from "child_process";
+import {SpawnSyncOptions, SpawnSyncReturns}   from "child_process";
+import * as C from "child_process";
 
 
 
@@ -13,14 +13,14 @@ import {SpawnSyncOptions, SpawnSyncReturns} from "child_process";
 export {ChildProcess} from "child_process";
 export {ChildProcessByStdio} from "child_process";
 export {ChildProcessWithoutNullStreams} from "child_process";
-export {CommonExecOptions} from "child_process";
-export {CommonOptions} from "child_process";
-export {CommonSpawnOptions} from "child_process";
-export {ExecException} from "child_process";
-export {ExecFileException} from "child_process";
-export {ExecFileOptions} from "child_process";
+export {CommonExecOptions}   from "child_process";
+export {CommonOptions}       from "child_process";
+export {CommonSpawnOptions}  from "child_process";
+export {ExecException}       from "child_process";
+export {ExecFileException}   from "child_process";
+export {ExecFileOptions}     from "child_process";
 export {ExecFileOptionsWithBufferEncoding} from "child_process";
-export {ExecFileOptionsWithOtherEncoding} from "child_process";
+export {ExecFileOptionsWithOtherEncoding}  from "child_process";
 export {ExecFileOptionsWithStringEncoding} from "child_process";
 export {ExecFileSyncOptions} from "child_process";
 export {ExecFileSyncOptionsWithBufferEncoding} from "child_process";
@@ -29,24 +29,24 @@ export {ExecOptions} from "child_process";
 export {ExecOptionsWithBufferEncoding} from "child_process";
 export {ExecOptionsWithStringEncoding} from "child_process";
 export {ForkOptions} from "child_process";
-export {IOType} from "child_process";
-export {MessageOptions} from "child_process";
-export {MessagingOptions} from "child_process";
+export {IOType}      from "child_process";
+export {MessageOptions}    from "child_process";
+export {MessagingOptions}  from "child_process";
 export {ProcessEnvOptions} from "child_process";
-export {PromiseWithChild} from "child_process";
-export {SendHandle} from "child_process";
-export {Serializable} from "child_process";
+export {PromiseWithChild}  from "child_process";
+export {SendHandle}        from "child_process";
+export {Serializable}      from "child_process";
 export {SerializationType} from "child_process";
-export {SpawnOptions} from "child_process";
+export {SpawnOptions}      from "child_process";
 export {SpawnOptionsWithStdioTuple} from "child_process";
-export {SpawnOptionsWithoutStdio} from "child_process";
-export {SpawnSyncOptions} from "child_process";
+export {SpawnOptionsWithoutStdio}   from "child_process";
+export {SpawnSyncOptions}  from "child_process";
 export {SpawnSyncOptionsWithBufferEncoding} from "child_process";
 export {SpawnSyncOptionsWithStringEncoding} from "child_process";
-export {SpawnSyncReturns} from "child_process";
-export {StdioNull} from "child_process";
-export {StdioOptions} from "child_process";
-export {StdioPipe} from "child_process";
+export {SpawnSyncReturns}  from "child_process";
+export {StdioNull}      from "child_process";
+export {StdioOptions}   from "child_process";
+export {StdioPipe}      from "child_process";
 export {StdioPipeNamed} from "child_process";
 
 
@@ -57,11 +57,11 @@ export {StdioPipeNamed} from "child_process";
 
 // export {exec} from "child_process";
 // export {execFile} from "child_process";
-export {execFileSync} from "child_process";
-export {execSync} from "child_process";
-export {fork} from "child_process";
 // export {spawn} from "child_process";
+export {execSync}  from "child_process";
+export {execFileSync} from "child_process";
 export {spawnSync} from "child_process";
+export {fork} from "child_process";
 
 
 
@@ -72,14 +72,44 @@ export {spawnSync} from "child_process";
 /** Return value from exec*Async(). */
 export interface ExecAsyncReturns {
   /** Standard output written by child process. */
-  stdout: string,
+  stdout: string | Buffer,
   /** Standard error written by child process. */
-  stderr: string,
+  stderr: string | Buffer,
 }
+
+/** Error from exec*Async(). */
+export interface ExecAsyncException extends ExecException, ExecAsyncReturns {}
 
 
 /** Callback for exec*(). */
-export type ExecCallback = (error: ExecException, stdout: string, stderr: string) => void;
+export type ExecCallback = (error: ExecException, stdout: string | Buffer, stderr: string | Buffer) => void;
+
+
+/** Callback and promise for exec*Async(). */
+class ExecAsyncHandler {
+  /** Resolve function of promise.  */
+  resolve: (output: ExecAsyncReturns) => void;
+  /** Reject function of promise. */
+  reject:  (error:  ExecAsyncException) => void;
+  /** Callback for exec*(). */
+  callback: ExecCallback;
+  /** Promise that resolves on completion */
+  promise:  Promise<ExecAsyncReturns>;
+
+  /** Define callback and promise for exec*Async(). */
+  constructor() {
+    this.resolve = null;
+    this.reject  = null;
+    this.callback = (err, stdout, stderr) => {
+      if (err != null) this.reject(Object.assign(err, {stdout, stderr}));
+      else this.resolve({stdout, stderr});
+    };
+    this.promise = new Promise((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject  = reject;
+    });
+  }
+}
 
 
 /**
@@ -89,16 +119,9 @@ export type ExecCallback = (error: ExecException, stdout: string, stderr: string
  * @returns output {stdout, stderr}
  */
 export function execAsync(command: string, options?: ExecSyncOptions): PromiseWithChild<ExecAsyncReturns> {
-  var fresolve = null, freject = null;
-  var child = C.exec(command, options, (err, stdout, stderr) => {
-    if (err != null) freject(Object.assign(err, {stdout, stderr}));
-    else fresolve({stdout, stderr});
-  });
-  var promise: Promise<ExecAsyncReturns> = new Promise((resolve, reject) => {
-    fresolve = resolve;
-    freject  = reject;
-  });
-  return Object.assign(promise, {child});
+  var e = new ExecAsyncHandler();
+  var child = C.exec(command, options, e.callback);
+  return Object.assign(e.promise, {child});
 }
 
 
@@ -119,7 +142,7 @@ export function exec(command: string, callback: ExecCallback): PromiseWithChild<
  */
 export function exec(command: string, options: ExecSyncOptions, callback: ExecCallback): PromiseWithChild<ExecAsyncReturns>;
 
-export function exec(command: string, options: ExecSyncOptions | ExecCallback, callback?: ExecCallback): C.ChildProcess | PromiseWithChild<ExecAsyncReturns> {
+export function exec(command: string, options: ExecSyncOptions | ExecCallback, callback?: ExecCallback): ChildProcess | PromiseWithChild<ExecAsyncReturns> {
   if (typeof callback==="function") return C.exec(command, options as ExecSyncOptions, callback);
   if (typeof options ==="function") return C.exec(command, options);
   return execAsync(command, options);
@@ -149,18 +172,11 @@ export function execFileAsync(file: string, options?: ExecFileSyncOptions): Prom
 export function execFileAsync(file: string, args: string[], options?: ExecFileSyncOptions): PromiseWithChild<ExecAsyncReturns>;
 
 export function execFileAsync(file: string, args?: string[] | ExecFileSyncOptions, options?: ExecFileSyncOptions): PromiseWithChild<ExecAsyncReturns> {
-  var fresolve = null, freject = null, child: ChildProcess = null;
-  var callback = (err: ExecException, stdout: string, stderr: string) => {
-    if (err != null) freject(err);
-    else fresolve({stdout, stderr});
-  };
-  if (typeof args==="undefined")  child = C.execFile(file, callback);
-  else if (args instanceof Array) child = C.execFile(file, args, options, callback);
-  var promise: Promise<ExecAsyncReturns> = new Promise((resolve, reject) => {
-    fresolve = resolve;
-    freject  = reject;
-  });
-  return Object.assign(promise, {child});
+  var e = new ExecAsyncHandler();
+  var _args    = args instanceof Array? args    : null;
+  var _options = args instanceof Array? options : args;
+  var child = C.execFile(file, _args, _options, e.callback);
+  return Object.assign(e.promise, {child});
 }
 
 
@@ -213,19 +229,45 @@ export function execFile(file: string, args: string[] | ExecFileSyncOptions | Ex
 // SPAWN
 // -----
 
-/** Default buffer limit. */
-const BUFFER_LIMIT = 1024 * 1024;
+/** Default maximum buffer size. */
+const MAX_BUFFER = 1024 * 1024;
 
 
-/**
- * Combine list of strings or buffers.
- * @param data list of strings or buffers
- * @param encoding data encoding (utf8, ...)
- * @returns combined string or buffer
- */
-function concatData(data: string[] | Buffer[], encoding: BufferEncoding | "buffer") {
-  if (data.length > 0) return typeof data[0]==="string"? data.join("") : Buffer.concat(data as Buffer[]);
-  return encoding!=null && encoding!=="buffer"?          data.join("") : Buffer.concat(data as Buffer[]);
+/** Encoding for stream. */
+type StreamEncoding = BufferEncoding | "buffer";
+
+
+/** Capturer of stream data. */
+class StreamBuffer {
+  /** Size of captured data. */
+  size: number;
+  /** Captured data. */
+  data: string[] | Buffer[];
+  /** Encoding of buffer. */
+  encoding: BufferEncoding | "buffer";
+
+  /**
+   * Capture stream data.
+   * @param stream readable stream
+   * @param maxBuffer maximum buffer size [MAX_BUFFER]
+   */
+  constructor(stream: NodeJS.ReadableStream, encoding: StreamEncoding="buffer", maxBuffer: number=MAX_BUFFER) {
+    this.size = 0;
+    this.data = [];
+    this.encoding = encoding;
+    if (encoding && encoding!=="buffer") stream.setEncoding(encoding);
+    stream.on("data", chunk => {
+      if (this.size + chunk.length > maxBuffer) return;
+      this.data.push(chunk);
+      this.size += chunk.length;
+    });
+  }
+
+  /** Get captured data of stream. */
+  value(): string | Buffer {
+    if (!this.encoding || this.encoding==="buffer") return Buffer.concat(this.data as Buffer[]);
+    return this.data.join("");
+  }
 }
 
 
@@ -247,37 +289,25 @@ export function spawnAsync(command: string, options?: SpawnSyncOptions): Promise
 export function spawnAsync(command: string, args: string[], options?: SpawnSyncOptions): PromiseWithChild<SpawnSyncReturns<string | Buffer>>;
 
 export function spawnAsync(command: string, args?: string[] | SpawnSyncOptions, options?: SpawnSyncOptions): PromiseWithChild<SpawnSyncReturns<string | Buffer>> {
-  var stdouts = [], stdout_size = 0;
-  var stderrs = [], stderr_size = 0;
-  var _options: SpawnSyncOptions = args instanceof Array? options : args;
-  var child: ChildProcess = null;
-  if (args instanceof Array) child = C.spawn(command, args, options);
-  else child = C.spawn(command, args as SpawnSyncOptions);
-  var encoding = _options && _options.encoding? _options.encoding : null;
-  if (encoding!=null && encoding!=="buffer") child.stdout.setEncoding(encoding);
-  if (encoding!=null && encoding!=="buffer") child.stderr.setEncoding(encoding);
-  child.stdout.on('data', chunk => {
-    if (stdout_size + chunk.length > BUFFER_LIMIT) return;
-    stdouts.push(chunk);
-    stdout_size += chunk.length;
-  });
-  child.stderr.on('data', chunk => {
-    if (stderr_size + chunk.length > BUFFER_LIMIT) return;
-    stderrs.push(chunk);
-    stderr_size += chunk.length;
-  });
+  var _args     = args instanceof Array? args    : null;
+  var _options  = args instanceof Array? options : args;
+  var encoding  = _options?.encoding  || "buffer";
+  var maxBuffer = _options?.maxBuffer || MAX_BUFFER;
+  var child  = C.spawn(command, _args, _options);
+  var outbuf = new StreamBuffer(child.stdout, encoding, maxBuffer);
+  var errbuf = new StreamBuffer(child.stderr, encoding, maxBuffer);
   var promise: Promise<SpawnSyncReturns<string | Buffer>> = new Promise((resolve, reject) => {
-    child.on('error', err => {
+    child.on("error", err => {
       var {pid, exitCode, signalCode} = child;
-      var stdout = concatData(stdouts, encoding);
-      var stderr = concatData(stderrs, encoding);
+      var stdout = outbuf.value();
+      var stderr = errbuf.value();
       var output = [null, stdout, stderr];
       reject(Object.assign(err, {pid, output, stdout, stderr, status: exitCode, signal: signalCode}));
     });
-    child.on('close', (code, signal) => {
+    child.on("close", (code, signal) => {
       var {pid} = child;
-      var stdout = concatData(stdouts, encoding);
-      var stderr = concatData(stderrs, encoding);
+      var stdout = outbuf.value();
+      var stderr = errbuf.value();
       var output = [null, stdout, stderr];
       resolve({pid, output, stdout, stderr, status: code, signal});
     });
