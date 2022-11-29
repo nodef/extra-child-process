2760f0e5fc52f5ecf346fff75f71f676c39c6ff7
Subhajit Sahu <wolfram77@gmail.com>
2022-11-29 23:53:23 +0530

    :bug: simplify imports, exports in src

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import {ExecException} from "child_process";
-import {ChildProcess, PromiseWithChild}       from "child_process";
-import {ExecSyncOptions, ExecFileSyncOptions} from "child_process";
-import {SpawnSyncOptions, SpawnSyncReturns}   from "child_process";
 import {Dirent} from "fs";
 import {EOL}    from "os";
+import {
+  ExecException,
+  ChildProcess,
+  PromiseWithChild,
+  ExecSyncOptions,
+  ExecFileSyncOptions,
+  SpawnSyncOptions,
+  SpawnSyncReturns,
+} from "child_process";
 import * as C from "child_process";
 import * as F from "fs";
 import * as P from "path";
@@ -14,44 +19,46 @@ import * as P from "path";
 // TYPES
 // =====
 
-export {ChildProcess} from "child_process";
-export {ChildProcessByStdio} from "child_process";
-export {ChildProcessWithoutNullStreams} from "child_process";
-export {CommonExecOptions}   from "child_process";
-export {CommonOptions}       from "child_process";
-export {CommonSpawnOptions}  from "child_process";
-export {ExecException}       from "child_process";
-export {ExecFileException}   from "child_process";
-export {ExecFileOptions}     from "child_process";
-export {ExecFileOptionsWithBufferEncoding} from "child_process";
-export {ExecFileOptionsWithOtherEncoding}  from "child_process";
-export {ExecFileOptionsWithStringEncoding} from "child_process";
-export {ExecFileSyncOptions} from "child_process";
-export {ExecFileSyncOptionsWithBufferEncoding} from "child_process";
-export {ExecFileSyncOptionsWithStringEncoding} from "child_process";
-export {ExecOptions} from "child_process";
-export {ExecOptionsWithBufferEncoding} from "child_process";
-export {ExecOptionsWithStringEncoding} from "child_process";
-export {ForkOptions} from "child_process";
-export {IOType}      from "child_process";
-export {MessageOptions}    from "child_process";
-export {MessagingOptions}  from "child_process";
-export {ProcessEnvOptions} from "child_process";
-export {PromiseWithChild}  from "child_process";
-export {SendHandle}        from "child_process";
-export {Serializable}      from "child_process";
-export {SerializationType} from "child_process";
-export {SpawnOptions}      from "child_process";
-export {SpawnOptionsWithStdioTuple} from "child_process";
-export {SpawnOptionsWithoutStdio}   from "child_process";
-export {SpawnSyncOptions}  from "child_process";
-export {SpawnSyncOptionsWithBufferEncoding} from "child_process";
-export {SpawnSyncOptionsWithStringEncoding} from "child_process";
-export {SpawnSyncReturns}  from "child_process";
-export {StdioNull}      from "child_process";
-export {StdioOptions}   from "child_process";
-export {StdioPipe}      from "child_process";
-export {StdioPipeNamed} from "child_process";
+export {
+  ChildProcess,
+  ChildProcessByStdio,
+  ChildProcessWithoutNullStreams,
+  CommonExecOptions,
+  CommonOptions,
+  CommonSpawnOptions,
+  ExecException,
+  ExecFileException,
+  ExecFileOptions,
+  ExecFileOptionsWithBufferEncoding,
+  ExecFileOptionsWithOtherEncoding,
+  ExecFileOptionsWithStringEncoding,
+  ExecFileSyncOptions,
+  ExecFileSyncOptionsWithBufferEncoding,
+  ExecFileSyncOptionsWithStringEncoding,
+  ExecOptions,
+  ExecOptionsWithBufferEncoding,
+  ExecOptionsWithStringEncoding,
+  ForkOptions,
+  IOType,
+  MessageOptions,
+  MessagingOptions,
+  ProcessEnvOptions,
+  PromiseWithChild,
+  SendHandle,
+  Serializable,
+  SerializationType,
+  SpawnOptions,
+  SpawnOptionsWithStdioTuple,
+  SpawnOptionsWithoutStdio,
+  SpawnSyncOptions,
+  SpawnSyncOptionsWithBufferEncoding,
+  SpawnSyncOptionsWithStringEncoding,
+  SpawnSyncReturns,
+  StdioNull,
+  StdioOptions,
+  StdioPipe,
+  StdioPipeNamed,
+} from "child_process";
 
 
 
@@ -59,13 +66,15 @@ export {StdioPipeNamed} from "child_process";
 // METHODS
 // =======
 
-// export {exec} from "child_process";
-// export {execFile} from "child_process";
-export {fork}  from "child_process";
-export {spawn} from "child_process";
-export {execSync}     from "child_process";
-export {execFileSync} from "child_process";
-export {spawnSync}    from "child_process";
+export {
+  // exec,
+  // execFile,
+  fork,
+  spawn,
+  execSync,
+  execFileSync,
+  spawnSync,
+} from "child_process";
 
 
 
